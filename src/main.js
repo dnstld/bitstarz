48f41ddbaa31e75d7262eeff3f41ee6a462ed8f7
Denis Toledo <contato@denistoledo.com.br>
2020-05-25 14:49:05 -0300

    register component globally

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
 import App from './App.vue';
 import router from './router';
+
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/,
+);
+
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, ''),
+    ),
+  );
+
+  Vue.component(
+    componentName,
+    componentConfig.default || componentConfig,
+  );
+});
 
 Vue.config.productionTip = false;
 
