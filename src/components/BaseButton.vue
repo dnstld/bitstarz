9c18e86f91643408393d6b929ea2b3949cdf87b0
Denis Toledo <contato@denistoledo.com.br>
2020-05-25 17:12:31 -0300

    create base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    :type="type"
+    :class="{
+      'btn-primary': primary,
+      'btn-secondary': secondary,
+      'btn-bordered': bordered,
+      'btn-lg': large,
+    }"
+    class="btn"
+  >
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    secondary: {
+      type: Boolean,
+      default: false,
+    },
+    bordered: {
+      type: Boolean,
+      default: false,
+    },
+    large: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
