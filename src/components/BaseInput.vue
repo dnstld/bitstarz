4b94aad4fa1d326f0295003ee0ce244f97fa5512
Denis Toledo <contato@denistoledo.com.br>
2020-05-26 22:33:43 -0300

    create base input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="base-input">
+    <input
+      :value="value"
+      @input="updateValue"
+      v-bind="$attrs"
+      class="base-input__field"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.base-input {
+  width: 100%;
+
+  &__field {
+    border: 1px solid;
+    border-radius: .5rem;
+    box-sizing: border-box;
+    font-size: $text-14;
+    padding: 1rem;
+    width: inherit;
+    @include color("background-color", "grey-200");
+    @include color("border-color", "grey-300");
+    @include color("color", "grey-700");
+  }
+}
+</style>
